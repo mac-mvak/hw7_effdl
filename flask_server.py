2057091485f41b3189d1229306edc714dd7466a3
mac-mvak <maximusev1290@gmail.com>
2024-03-17 23:02:09 +0300

    Remove redundant.

--- flask_server.py
+++ flask_server.py
@@ -1,29 +1,18 @@
-import requests
-import torch
-import io
 import grpc
 import inference_pb2_grpc
 import inference_pb2
 
 
 from flask import Flask, request, jsonify
-from torchvision.models.detection import maskrcnn_resnet50_fpn, MaskRCNN_ResNet50_FPN_Weights
 from prometheus_flask_exporter import PrometheusMetrics
 
 
-from PIL import Image
-from torchvision.transforms import ToTensor
 
 
 
 app = Flask(__name__, static_url_path="")
 metrics = PrometheusMetrics(app)
 
-
-model = maskrcnn_resnet50_fpn(weights=MaskRCNN_ResNet50_FPN_Weights.COCO_V1)
-names = MaskRCNN_ResNet50_FPN_Weights.COCO_V1.meta['categories']
-model.eval()
-to_tensor = ToTensor()
 
 
 @app.route("/predict", methods=['POST'])
